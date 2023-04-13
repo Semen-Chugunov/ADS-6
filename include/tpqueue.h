a11072a03a64a2af6024ab0447dd0a62c3ba4aa8
Semen-Chugunov <125124591+Semen-Chugunov@users.noreply.github.com>
2023-04-13 09:53:18 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,45 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+    T* arr;
+    uint64_t f, l, c;
+
+ public:
+    TPQueue(): f(0), l(0), c(0) {
+        arr = new T[size];
+    }
+    bool full() const {
+        return c == size;
+    }
+    bool empt() const {
+        return c == 0;
+    }
+    void push(const T& val) {
+        if (full()) {
+            throw std::string("full");
+        } else {
+            c++;
+            int id = l;
+            for (int i = f; i < l; i++) {
+                if (arr[i].prior < val.prior) {
+                    id = i;
+                    break;
+                }
+            }
+            for (int i = l; i > id; i--) {
+                arr[i % size] = arr[(i - 1) % size];
+            }
+            arr[id % size] = val;
+            l++;
+        }
+    }
+    T& pop() {
+        if (empt()) 
+            throw std::string("empty");
+        c--;
+        return arr[f++ % size];
+    }
 };
 
 struct SYM {
